d7a9249e1d4a50278ebed2ee4468c36230dadfcc
Alessandra Gherardelli <AlexGherardelli@users.noreply.github.com>
2024-04-09 15:04:41 +0200

    Working Sqlalchemy file

---
+++ playground.py
@@ -0,0 +1,34 @@
+import os
+import pyodbc
+import pandas as pd
+from sqlalchemy import create_engine 
+from dotenv import load_dotenv
+
+
+if __name__ == "__main__":
+    # sample data
+    sample_data = {'col1': [1, 2, 3], 'col2': ['a', 'b', 'c']}
+    df = pd.DataFrame(sample_data)
+
+    load_dotenv()  # take environment variables from .env.
+
+    SERVER = os.getenv("SERVER")
+    DATABASE = os.getenv("DB_NAME")
+    USERNAME = os.getenv("DB_USERNAME")
+    PASSWORD = os.getenv("DB_PASSWORD")
+
+    # conn_str = f'DRIVER={{SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}'
+    conn_str = f'mssql+pyodbc://{USERNAME}:{PASSWORD}@{SERVER}/{DATABASE}?driver=ODBC+Driver+17+for+SQL+Server'
+    engine = create_engine(conn_str)
+
+    sql = """
+    SELECT * FROM [dbo].IpcValues
+    """
+
+    sql_df = pd.read_sql( 
+    sql, 
+    con=engine 
+) 
+    print(sql_df.head())
+    
+
