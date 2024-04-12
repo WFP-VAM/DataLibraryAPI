59912e3019c0281bd2d4c5823d16e9456ac5701c
Alessandra Gherardelli <AlexGherardelli@users.noreply.github.com>
2024-04-12 12:09:17 +0200

    Fixing dataframes with weird JSON formatting

--- main.py
+++ main.py
@@ -4,23 +4,25 @@ from dotenv import load_dotenv
 from datetime import date
 import pandas as pd
 import os
+import json
 import logging
 
 logger = logging.getLogger(__name__)
 
 load_dotenv()  # take environment variables from .env.
 
+
 def get_data_from_api():
-
     """
-    Get data from DL api and returns two dataframes
-    """
+    Get data from DL api and returns two dataframes.
 
+    This function initializes a DataLibrary API instance, retrieves the survey list and user information from the API, and returns two dataframes - one containing all surveys with resources, and one containing user information.
+
+    Returns:
+        Tuple[pd.DataFrame, pd.DataFrame]: A tuple containing the dataframes for surveys with resources and users.
+    """
     # initiate Data Library API instance
     dl = DataLibrary(os.getenv("DATALIB_API_KEY"))
-
-    # call the help function to get basic info about usage
-    # urls = dl.help()
 
     # get survey list in format DATE_ISO3_SURVEYTYPE or DATEISO3SURVEYTYPE
     survey_list = dl.get_survey_list()
@@ -37,22 +39,24 @@ def get_data_from_api():
 
     # get all information about surveys
     all_surveys_with_resources = dl.get_surveys_with_resources(limit=total_surveys)
-    all_surveys_with_resources =  pd.json_normalize(all_surveys_with_resources, max_level=0)
-    
-    
+
+    # with open('all_surveys_with_resources.json', 'w') as f:
+    #     json.dump(all_surveys_with_resources, f)
+    all_surveys_with_resources =  pd.DataFrame.from_records(all_surveys_with_resources)
+
     return  (all_surveys_with_resources, users)
 
-def export_data_to_csv(api_data):
+def export_data_to_csv(data: tuple):
     # export survey list, survey information with resources and user list as csv
-    all_filenames = ["datalib_all_info", "datalib_users"]
+    all_filenames = ["surveys", "users", "resources"]
     folder = "output"
     today = str(date.today()).replace("-", "_")
 
-    for df, filename in zip(api_data, all_filenames):
+    for df, filename in zip(data, all_filenames):
         path = f"{folder}/{today}_{filename}.csv"
         df.to_csv(path)
 
-def load_to_db(data):
+def load_to_db(data: tuple):
     table_names = ("DL_Surveys", "DL_Resources", "DL_Users")
     try: 
         for df, table_name in zip(data, table_names):
@@ -62,8 +66,8 @@ def load_to_db(data):
         logger.error(f"Error loading data: {e}")
 
 def process_data(data):
-    raw_surveys_with_resources, users = api_data
-    processed_surveys_with_resources = raw_surveys_with_resources[['assessment_status', 'collection_method',
+    surveys, users = data
+    processed_surveys_with_resources = surveys[['assessment_status', 'collection_method',
     'creator_user_id', 'data_collector', 'description', 'end_date', 'id',
     'metadata_created', 'metadata_modified', 'month',
     'name', 'notes', 'num_resources', 'organization_id',
@@ -74,41 +78,28 @@ def process_data(data):
     'survey_attributes', 'survey_category', 'survey_type', 'title', 'type',
         'year', 'resources']]
 
-    processed_surveys_with_resources = processed_surveys_with_resources.rename(columns={"'organization_id": "container_id", 'organization_title': 'container_title', 'organization_type': 'container_type', 'organization_description': 'container_description', 'organization_created': 'container_created' })
+    # processed_surveys_with_resources = processed_surveys_with_resources.rename(columns={"'organization_id": "container_id", 'organization_title': 'container_title', 'organization_type': 'container_type', 'organization_description': 'container_description', 'organization_created': 'container_created' })
+
+
+    contain_values = processed_surveys_with_resources[processed_surveys_with_resources['assessment_status'].str.contains("'")]
+    print(contain_values)
 
     resources = processed_surveys_with_resources[["resources", "container_id"]]
 
     return (processed_surveys_with_resources, resources, users)
 
 
-
-def main():
-    """Get user and survey data from Data Library and create CSV files for each of the datasets."""
-
-    # Get users and surveys information
+if __name__== "__main__":
     api_data = get_data_from_api()
 
-    # Export information to CSV
-    export_data_to_csv(api_data)
-
     # Load processed data to DB
     processed_data = process_data(api_data)
-    # load_to_db(processed_data)
-
-
-    # Success!
-    print("\nAll data saved!")
-
-if __name__== "__main__":
-        api_data = get_data_from_api()
 
     # Export information to CSV
-    export_data_to_csv(api_data)
-
-    # Load processed data to DB
-    processed_data = process_data(api_data)
+    export_data_to_csv(processed_data)
     # load_to_db(processed_data)
 
+    print("Done")
 
 
 
