7f8b60859158179232549fb55409f40b01776a21
AlexGherardelli <alessandra.gherardelli@gmail.com>
2026-06-11 12:19:41 +0200

    fix: importing modules in __init__.py

--- datalibrary/__init__.py
+++ datalibrary/__init__.py
@@ -6,4 +6,10 @@ from .extract import (
     get_user_data,
 )
 
-__all__ = ["DataLibrary"]
+__all__ = [
+    "DataLibrary",
+    "get_data",
+    "get_member_data",
+    "get_survey_data",
+    "get_user_data",
+]
